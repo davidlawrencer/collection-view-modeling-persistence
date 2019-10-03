781b69d2866ac4c8756ca19afdb802aff3177439
davidlawrencer <davidlrifkin+github@gmail.com>
2019-10-03 10:29:30 -0400

    Rearranges files, adds Dog model

---
+++ collection-view-with-persistence/Dog.swift
@@ -0,0 +1,20 @@
+//
+//  Dog.swift
+//  collection-view-with-persistence
+//
+//  Created by David Rifkin on 10/3/19.
+//  Copyright © 2019 David Rifkin. All rights reserved.
+//
+
+import Foundation
+
+struct Dog {
+    let breed: String
+    let image: String
+    let id: Int
+    
+    static func getDogs() -> [Dog] {
+        return [ Dog(breed:"collie", image: "collie", id: 1 ), Dog(breed: "Chinese Tibetan Mastiff", image: "chinese-tibetan-mastiff", id: 2)
+        ]
+    }
+}
