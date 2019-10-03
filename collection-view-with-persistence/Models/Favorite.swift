837458794d96ec82af208e99b4741d9acee4b601
davidlawrencer <davidlrifkin+github@gmail.com>
2019-10-03 10:34:54 -0400

    Adds favorite object, rearrages files into folders

---
+++ collection-view-with-persistence/Models/Favorite.swift
@@ -0,0 +1,13 @@
+//
+//  Favorite.swift
+//  collection-view-with-persistence
+//
+//  Created by David Rifkin on 10/3/19.
+//  Copyright © 2019 David Rifkin. All rights reserved.
+//
+
+import Foundation
+
+struct Favorite {
+    let dogID: Int
+}
